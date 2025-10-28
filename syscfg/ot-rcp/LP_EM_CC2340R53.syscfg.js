65e309d475155505e73345d1aed0a79a887e96a7
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-10-28 17:35:11 +0100

    fix: use defaults for LP_EM_CC2340R53

--- syscfg/ot-rcp/LP_EM_CC2340R53.syscfg.js
+++ syscfg/ot-rcp/LP_EM_CC2340R53.syscfg.js
@@ -1,8 +1,1 @@
 // LP_EM_CC2340R53
-
-Device.enableBootloader = true;
-Device.enableBootloaderBackdoor = true;
-Device.levelBootloaderBackdoor = "Active low";
-// Device.dioBootloaderBackdoor = 15; // TODO
-
-RFCustom.radioConfigieee154.txPower = "5";
